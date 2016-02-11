2a7521d32676bb50e61e8a29672f9df45269075e
Bhuvnesh Kumar <bhuvnesh.kumar@paxcel.net>
2016-02-11 14:14:33 +0530

    FirstTestApp/routes/index.js

--- FirstTestApp/routes/index.js
+++ FirstTestApp/routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 router.get('/login', function(req, res, next) {
+  console.log("Login api");	
   res.send("Login screen");
 });
 
